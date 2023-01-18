cb851589cbfa3b6cc2b0e2d5db30464dc31d1366
Glenn Ladrido <glenn.ladrido@gmail.com>
2023-01-18 09:39:31 +0800

    created navbar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div className="text-3xl font-bold underline">Hello World</div>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      display: ["Dela Gothic One", "cursive"],
+    },
     extend: {},
   },
   plugins: [],
